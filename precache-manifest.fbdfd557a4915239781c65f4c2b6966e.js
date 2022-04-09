0c4601dd0150b689d6e51d527574a972436221f4
vidhu <vidhu.v9@gmail.com>
2022-04-09 20:39:34 +0530

    Updates

---
+++ precache-manifest.fbdfd557a4915239781c65f4c2b6966e.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f2fb3b328ab1418c2622fcb15ba6fe73",
+    "url": "./index.html"
+  },
+  {
+    "revision": "d276a8d75c5bd83cd62e",
+    "url": "./static/css/2.a6a636a0.chunk.css"
+  },
+  {
+    "revision": "8c74f8ea06350cf95c0d",
+    "url": "./static/css/main.d850c1a7.chunk.css"
+  },
+  {
+    "revision": "d276a8d75c5bd83cd62e",
+    "url": "./static/js/2.ea07e3d4.chunk.js"
+  },
+  {
+    "revision": "21b2dd38606ac28751db0d936591a095",
+    "url": "./static/js/2.ea07e3d4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8c74f8ea06350cf95c0d",
+    "url": "./static/js/main.ff0a1f2f.chunk.js"
+  },
+  {
+    "revision": "e720711dd3e4e8b209d0",
+    "url": "./static/js/runtime-main.43c7aded.js"
+  },
+  {
+    "revision": "df8b84d5c24a3544c25001f3959d7a77",
+    "url": "./static/media/box.df8b84d5.png"
+  }
+]);
